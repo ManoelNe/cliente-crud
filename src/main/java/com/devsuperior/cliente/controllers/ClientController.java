db7370f3c9bcb4c6de2ad0ca07e633d6e69b42c0
ManoelNe <manoel_neto@outlook.com>
2026-02-27 19:21:51 -0300

    feat: Post Client

--- src/main/java/com/devsuperior/cliente/controllers/ClientController.java
+++ src/main/java/com/devsuperior/cliente/controllers/ClientController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,11 @@ public class ClientController {
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
         Page<ClientDTO> page = service.findAll(pageable);
         return ResponseEntity.ok(page);
+    }
+
+    @PutMapping
+    public ClientDTO insert(@RequestBody ClientDTO dto){
+        return service.insert(dto);
     }
 
 }
--- src/main/java/com/devsuperior/cliente/services/ClientService.java
+++ src/main/java/com/devsuperior/cliente/services/ClientService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class ClientService {
@@ -26,5 +27,21 @@ public class ClientService {
         Page<Client> result = repository.findAll(pageable);
         return result.map(x-> new ClientDTO(x));
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+
+    }
+
 
 }
