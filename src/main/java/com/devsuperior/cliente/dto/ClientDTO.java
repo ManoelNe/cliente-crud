0b7d57e87309ccbda04d7b2c55413ed684d68ac8
ManoelNe <manoel_neto@outlook.com>
2026-02-27 17:00:12 -0300

    feat: cria ClientDTO

---
+++ src/main/java/com/devsuperior/cliente/dto/ClientDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.cliente.dto;
+
+
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(){
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
