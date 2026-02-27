25efaae2e1fc1fef53eab5744fe7d616c4c5b8d9
ManoelNe <manoel_neto@outlook.com>
2026-02-27 20:39:48 -0300

    feat: Put Client

--- src/main/java/com/devsuperior/cliente/controllers/ClientController.java
+++ src/main/java/com/devsuperior/cliente/controllers/ClientController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +27,22 @@ public class ClientController {
     public ClientDTO findById(@PathVariable Long id){
         return service.findById(id);
     }
+
     @GetMapping
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
         Page<ClientDTO> page = service.findAll(pageable);
         return ResponseEntity.ok(page);
     }
 
-    @PutMapping
+    @PostMapping
     public ClientDTO insert(@RequestBody ClientDTO dto){
         return service.insert(dto);
+    }
+
+    @PutMapping(value = "/{id}" )
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 
 }
